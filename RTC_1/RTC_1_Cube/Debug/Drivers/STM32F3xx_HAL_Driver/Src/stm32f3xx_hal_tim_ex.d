4ef9dda904a2b63d4a40466ee3a6a91e144a7131
borsukvasyl <v.borsuk2000@gmail.com>
2017-11-16 15:49:04 +0200

    reading data from RTC

---
+++ RTC_1/RTC_1_Cube/Debug/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.d
@@ -0,0 +1,77 @@
+Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.o: \
+ ../Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Inc/stm32f3xx_hal_conf.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Inc/main.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Include/core_cm4.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Include/core_cmInstr.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Include/cmsis_gcc.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Include/core_cmFunc.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Include/core_cmSimd.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Inc/stm32f3xx_hal_conf.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Inc/main.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Include/core_cm4.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Include/core_cmInstr.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Include/core_cmFunc.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Include/core_cmSimd.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+
+E:/Vasyl/Programs/STM32F303/RTC/RTC_1/RTC_1_Cube/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:

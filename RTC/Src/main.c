c96db6e07929716ccabf81f7b31b82994a2de8a6
Marian Petruk <marian.petruk@gmail.com>
2018-01-15 13:14:41 +0200

    code beautify

--- RTC/Src/main.c
+++ RTC/Src/main.c
@@ -96,13 +96,13 @@ void SystemClock_Config(void);
 /* USER CODE BEGIN 0 */
 
 typedef struct {
-    uint8_t seconds; //Seconds parameter, from 00 to 59
-    uint8_t minutes; //Minutes parameter, from 00 to 59
-    uint8_t hours;   //Hours parameter, 24Hour mode, 00 to 23
-    uint8_t weekday; //Day in a week, from 1 to 7
-    uint8_t date;    //Date in a month, 1 to 31
-    uint8_t month;   //Month in a year, 1 to 12
-    uint8_t year;    //Year parameter
+    uint8_t seconds; // Seconds parameter, from 00 to 59
+    uint8_t minutes; // Minutes parameter, from 00 to 59
+    uint8_t hours;   // Hours parameter, 24Hour mode, 00 to 23
+    uint8_t weekday; // Day in a week, from 1 to 7
+    uint8_t date;    // Date in a month, 1 to 31
+    uint8_t month;   // Month in a year, 1 to 12
+    uint8_t year;    // Year parameter
 } DS3231_Time;
 
 inline int bcd_to_decimal(uint8_t bcd_value) {
@@ -236,123 +236,116 @@ int read_user_data(DS3231_Time* time) {
 
 /* USER CODE END 0 */
 
-int main(void)
-{
+int main(void){
+    /* USER CODE BEGIN 1 */
+    //initialise_monitor_handles();
 
-  /* USER CODE BEGIN 1 */
-  //initialise_monitor_handles();
+    /* USER CODE END 1 */
 
-  /* USER CODE END 1 */
+    /* MCU Configuration----------------------------------------------------------*/
 
-  /* MCU Configuration----------------------------------------------------------*/
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE END Init */
 
-  /* USER CODE END Init */
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE END SysInit */
 
-  /* USER CODE END SysInit */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_I2C1_Init();
+    MX_SPI2_Init();
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C1_Init();
-  MX_SPI2_Init();
+    /* USER CODE BEGIN 2 */
 
-  /* USER CODE BEGIN 2 */
+    lcd1.hw_conf.spi_handle = &hspi2;
+    lcd1.hw_conf.spi_cs_pin = LCD_CS_Pin;
+    lcd1.hw_conf.spi_cs_port = LCD_CS_GPIO_Port;
+    lcd1.hw_conf.rst_pin =  LCD_RST_Pin;
+    lcd1.hw_conf.rst_port = LCD_RST_GPIO_Port;
+    lcd1.hw_conf.dc_pin =  LCD_DC_Pin;
+    lcd1.hw_conf.dc_port = LCD_DC_GPIO_Port;
+    lcd1.def_scr = lcd5110_def_scr;
+    LCD5110_init(&lcd1.hw_conf, LCD5110_NORMAL_MODE, 0x40, 2, 3);
 
-  lcd1.hw_conf.spi_handle = &hspi2;
-  lcd1.hw_conf.spi_cs_pin = LCD_CS_Pin;
-  lcd1.hw_conf.spi_cs_port = LCD_CS_GPIO_Port;
-  lcd1.hw_conf.rst_pin =  LCD_RST_Pin;
-  lcd1.hw_conf.rst_port = LCD_RST_GPIO_Port;
-  lcd1.hw_conf.dc_pin =  LCD_DC_Pin;
-  lcd1.hw_conf.dc_port = LCD_DC_GPIO_Port;
-  lcd1.def_scr = lcd5110_def_scr;
-  LCD5110_init(&lcd1.hw_conf, LCD5110_NORMAL_MODE, 0x40, 2, 3);
+    DS3231_Time time;
+    read_user_data(&time);
+    RTC_write_data(&time);
 
-  DS3231_Time time;
-  read_user_data(&time);
-  RTC_write_data(&time);
+    /* USER CODE END 2 */
 
-  /* USER CODE END 2 */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1){
+    /* USER CODE END WHILE */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-  /* USER CODE END WHILE */
+        /* USER CODE BEGIN 3 */
 
-  /* USER CODE BEGIN 3 */
+        RTC_read_data(&time);
 
-    RTC_read_data(&time);
-
-    LCD5110_display_time(&time);
-    HAL_Delay(33);
-  }
-  /* USER CODE END 3 */
+        LCD5110_display_time(&time);
+        HAL_Delay(33);
+    }
+        /* USER CODE END 3 */
 
 }
 
 /** System Clock Configuration
 */
-void SystemClock_Config(void)
-{
+void SystemClock_Config(void){
 
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_PeriphCLKInitTypeDef PeriphClkInit;
-
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = 16;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
     /**Initializes the CPU, AHB and APB busses clocks 
     */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = 16;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK){
+        _Error_Handler(__FILE__, __LINE__);
+    }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK){
+        _Error_Handler(__FILE__, __LINE__);
+    }
 
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
-  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
+    PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK){
+        _Error_Handler(__FILE__, __LINE__);
+    }
 
     /**Configure the Systick interrupt time 
     */
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
     /**Configure the Systick 
     */
-  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
-  /* SysTick_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+    /* SysTick_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
 /* USER CODE BEGIN 4 */
@@ -364,14 +357,11 @@ void SystemClock_Config(void)
   * @param  None
   * @retval None
   */
-void _Error_Handler(char * file, int line)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  while(1) 
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */ 
+void _Error_Handler(char * file, int line){
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    while(1){}
+    /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef USE_FULL_ASSERT
@@ -383,12 +373,11 @@ void _Error_Handler(char * file, int line)
    * @param line: assert_param error line source number
    * @retval None
    */
-void assert_failed(uint8_t* file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
+void assert_failed(uint8_t* file, uint32_t line){
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line number,
     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE END 6 */
 
 }
 
